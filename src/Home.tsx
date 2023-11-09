5f7aee899e755361d4aeff1f72e4c3daac61fd28
Yannick Van Damme <yannickvandamme.yvd56@gmail.com>
2023-11-09 13:33:51 +0100

    basis map

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,5 +1,20 @@
+import { LatLngExpression } from "leaflet";
+import { MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
+
+const position: LatLngExpression = [51.505, -0.09]
+
 const Home = () => {
-    return <h1>Home</h1>;
-  };
+    return <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <Marker position={position}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
+  </MapContainer>
+  }
   
   export default Home;
